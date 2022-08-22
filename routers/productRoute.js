c9d7b5bb1f089c8176e9a309bb93df8b89336d44
Lucas Bispo Menezes <skaliboo1@gmail.com>
2022-08-22 16:17:13 -0300

    [feat] requisto 3 ok

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -1,8 +1,9 @@
 const productRote = require('express').Router();
 const productController = require('../controllers/productsController');
+const { validateCreator } = require('../middlewares/validate');
 
 productRote.get('/', productController.getAll);
 productRote.get('/:id', productController.findById);
-productRote.post('/', productController.create);
+productRote.post('/', validateCreator, productController.create);
 
 module.exports = productRote;
